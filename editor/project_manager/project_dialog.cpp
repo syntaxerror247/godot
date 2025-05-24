4248221068db17432ec0de15fa598b3b8c1907f7
Anish Mishra <mishragames@gmail.com>
2025-05-24 22:55:11 +0530

    Disable `nomedia` file creation on Android 11

--- editor/project_manager/project_dialog.cpp
+++ editor/project_manager/project_dialog.cpp
@@ -739,14 +739,17 @@ void ProjectDialog::ok_pressed() {
 	hide();
 	if (mode == MODE_NEW || mode == MODE_IMPORT || mode == MODE_INSTALL) {
 #ifdef ANDROID_ENABLED
-		// Create a .nomedia file to hide assets from media apps on Android.
-		const String nomedia_file_path = path.path_join(".nomedia");
-		Ref<FileAccess> f2 = FileAccess::open(nomedia_file_path, FileAccess::WRITE);
-		if (f2.is_null()) {
-			// .nomedia isn't so critical.
-			ERR_PRINT("Couldn't create .nomedia in project path.");
-		} else {
-			f2->close();
+		var build_version = JavaClassWrapper::get_singleton()->wrap("android.os.Build$VERSION");
+		if (build_version.SDK_INT != 30) {
+			// Create a .nomedia file to hide assets from media apps on Android.
+			const String nomedia_file_path = path.path_join(".nomedia");
+			Ref<FileAccess> f2 = FileAccess::open(nomedia_file_path, FileAccess::WRITE);
+			if (f2.is_null()) {
+				// .nomedia isn't so critical.
+				ERR_PRINT("Couldn't create .nomedia in project path.");
+			} else {
+				f2->close();
+			}
 		}
 #endif
 		emit_signal(SNAME("project_created"), path, edit_check_box->is_pressed());
