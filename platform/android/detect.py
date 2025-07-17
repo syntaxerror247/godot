6cdf65d2e7cea0b06afafc70a19309e06fd99f0d
Anish Mishra <mishragames@gmail.com>
2025-07-17 21:07:06 +0530

    a

--- platform/android/detect.py
+++ platform/android/detect.py
@@ -21,7 +21,7 @@ def get_opts():
 
     return [
         ("ANDROID_SDK_ROOT", "Path to the Android SDK", get_env_android_sdk_root()),
-        ("ndk_platform", 'Target platform (android-<api>, e.g. "android-19")', "android-19"),
+        ("ndk_platform", 'Target platform (android-<api>, e.g. "android-21")', "android-21"),
         EnumVariable("android_arch", "Target architecture", "armv7", ("armv7", "arm64v8", "x86", "x86_64")),
         BoolVariable("android_neon", "Enable NEON support (armv7 only)", True),
         BoolVariable("store_release", "Editor build for Google Play Store (for official builds only)", False),
@@ -43,7 +43,7 @@ def get_android_ndk_root(env):
 
 # This is kept in sync with the value in 'platform/android/java/app/config.gradle'.
 def get_ndk_version():
-    return "28.1.13356709"
+    return "28.2.13676358"
 
 
 def get_flags():
